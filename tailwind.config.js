94c5fc510bcc4163c890b39e0482309562f82d40
Alexandra Staszewski <alexandra_sta@yahoo.de>
2025-01-14 17:33:13 +0100

    Content Load Error Handling

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,24 +15,21 @@ module.exports = {
         "green-1": "#C1FF72",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%",
-        "mobile-home": "url('./assets/HomePageGraphic.png')",
-        "hero-pattern": "url('./assets/HeroPattern.png')",
-        "silver-pattern": "url('./assets/SilverPattern.png')"
+        "hero-pattern": "url('/assets/HeroPattern.png')",
       }),
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
         audiowide: ["Audiowide", "sans-serif"]
       },
       content: {
-        arrow: "url('./assets/Arrow.png')",
-        abstractwaves: "url('./assets/AbstractWaves.png')",
-        network: "url('./assets/Network.png')",
-        star: "url('./assets/Star.png')",
-        flash: "url('./assets/Flash.png')",
-        pink1: "url('./assets/Pink1.png')",
-        pinktropfen: "url('./assets/pink-tropfen.png')",
-        pinkherz: "url('./assets/pink-herz.png')",
+        arrow: "url('/assets/Arrow.png')",
+        abstractwaves: "url('/assets/AbstractWaves.png')",
+        network: "url('/assets/Network.png')",
+        star: "url('/assets/Star.png')",
+        flash: "url('/assets/Flash.png')",
+        pink1: "url('/assets/Pink1.png')",
+        pinktropfen: "url('/assets/pink-tropfen.png')",
+        pinkherz: "url('/assets/pink-herz.png')",
       },
       transform: {
         'rotate-y-0': 'rotateY(0deg)',
